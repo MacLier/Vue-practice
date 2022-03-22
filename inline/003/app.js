d5901e03f9df3248832487b393e12bc506cf86d9
MacLier <75802094+MacLier@users.noreply.github.com>
2022-03-22 18:58:43 +0100

    feat: working with watchers

--- inline/003/app.js
+++ inline/003/app.js
@@ -1,45 +1,50 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-      name: "",
-      confirmedName: "",
-    };
-  },
-  computed: {
-    fullname() {
-      if (this.name === '') {
-        return '';
-      }
-      return this.name + ' ' + "Doe"
-    }
-  },
-  methods: {
-    outputFullname() {
-      if (this.name === '') {
-        return '';
-      }
-      return this.name + ' ' + "Doe"
-    },
+	data() {
+		return {
+			counter: 0,
+			name: "",
+			lastName: "",
+		};
+	},
+	watch: {
+		counter(value) {
+			if (value > 50) {
+				const that = this;
+				setTimeout(() => {
+					that.counter = 0;
+				}, 2000)
+			}
+		},
+		// name(value) {
+		// 	if (value === '') this.fullname = '';
+		// 	else this.fullname = value + ' ' + this.lastName;
+		// },
+		// lastName(value) {
+		// 	if (value === '') this.fullname = '';
+		// 	else this.fullname = this.name + ' ' + value;
+		// },
+	},
+	computed: {
+		fullname() {
+			if (this.name === '' || this.lastName === '') {
+				return '';
+			} else {
+				return this.name + ' ' + this.lastName;
+			}
+		},
+	},
+	methods: {
 
-    setName(event) {
-      this.name = event.target.value;
-    },
-
-    submitForm(event) {
-      alert('Submitted!')
-    },
-
-    increaseCounter(num) {
-      this.counter += num;
-    },
-    decreaseCounter(num) {
-      this.counter -= num;
-    },
-    resetNameInput() {
-      this.name = '';
-    },
-  }
+		increaseCounter(num) {
+			this.counter += num;
+		},
+		decreaseCounter(num) {
+			this.counter -= num;
+		},
+		resetNameInput() {
+			this.name = '';
+		},
+	},
 });
 
 app.mount('#events');
