103519f5bc69fe40d484738cf31f7b41a43938ab
MacLier <75802094+MacLier@users.noreply.github.com>
2022-03-23 17:22:02 +0100

    feat: if, else if, else in html

--- inline/008/app.js
+++ inline/008/app.js
@@ -1,7 +1,20 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      goalInput: '',
+      goals: [],
+    };
   },
+  watch: {},
+  computed: {},
+  methods: {
+
+    addGoal() {
+      this.goals.push(this.goalInput);
+      this.goalInput = '';
+    },
+  },
+
 });
 
 app.mount('#user-goals');
