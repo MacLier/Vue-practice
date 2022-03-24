61d2d13170f5ca52bdb094f7412dae72102d9d71
MacLier <75802094+MacLier@users.noreply.github.com>
2022-03-24 13:46:36 +0100

    feat: FriendContact component & styleing added

---
+++ Vue-CLI/002/src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+  <li>
+    <h3>{{ friend.name }}</h3>
+    <button @click="toggleVisibility">
+      {{ detailsAreVisible ? "Hide" : "Show" }} details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone: </strong> {{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: {
+        id: "ali",
+        name: "Aladin Lopez",
+        phone: "012345678991",
+        email: "aliezdont@lop.com",
+      },
+    };
+  },
+  methods: {
+    toggleVisibility() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
--- Vue-CLI/002/src/main.js
+++ Vue-CLI/002/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue';
 
-import App from './App.vue'
+import App from './App.vue';
+import FriendContact from './components/FriendContact.vue';
 
-createApp(App).mount('');
+
+const app = createApp(App);
+
+app.component('friend-contact', FriendContact)
+
+app.mount('#app');
