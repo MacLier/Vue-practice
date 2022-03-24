67d11d26f0440e7105de1d960ec0284f36b6cc1d
MacLier <75802094+MacLier@users.noreply.github.com>
2022-03-24 13:24:37 +0100

    feat: app.vue added & bind

---
+++ Vue-CLI/002/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h2>My Friends</h2>
+  <ul>
+    <li></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "ali",
+          name: "Aladin Lopez",
+          phone: "012345678991",
+          email: "aliezdont@lop.com",
+        },
+        {
+          id: "pali",
+          name: "Pali Nemlopez",
+          phone: "012345678991",
+          email: "howeverpali@dontlopez.com",
+        },
+      ],
+    };
+  },
+};
+</script>
