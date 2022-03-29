70a9db95e33f7ab002b5e3ccfb324b4ce266e729
MacLier <75802094+MacLier@users.noreply.github.com>
2022-03-29 18:57:23 +0200

    feat: two component comminicate to each other

---
+++ Vue-CLI/003 - solved/src/components/UserData.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <form @submit.prevent="submitData">
+      <label for="name">Name</label>
+      <input
+        type="text"
+        id="name"
+        placeholder="Your Name"
+        v-model="enteredName"
+      />
+      <label for="age">Age</label>
+      <input
+        type="text"
+        id="age"
+        placeholder=" Your Age"
+        v-model="enteredAge"
+      />
+      <button>Set User Data</button>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  emits: ["set-data"],
+  data() {
+    return {
+      enteredName: "",
+      enteredAge: "",
+    };
+  },
+  methods: {
+    submitData() {
+      this.$emit("set-data", this.enteredName, this.enteredAge);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
