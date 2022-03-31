f5beaf14e34d09df78055f66056bd1b0a3c82dee
MacLier <75802094+MacLier@users.noreply.github.com>
2022-03-31 17:56:02 +0200

    feat: error modal with slot

--- Vue-CLI/005 Compnent Deep Dive/src/components/ManageGoals.vue
+++ Vue-CLI/005 Compnent Deep Dive/src/components/ManageGoals.vue
@@ -1,4 +1,35 @@
 <template>
-  <h2>Manage Goals</h2>
-  <input type="text" />
+  <div>
+    <h2>Manage Goals</h2>
+    <input type="text" ref="goal" />
+    <button @click="setGoal">Set Goal</button>
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is invalid!</h2>
+      <p>Please enter at least a few characters.</p>
+      <button @click="confirmError">Noted</button>
+    </error-alert>
+  </div>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: { ErrorAlert },
+  data() {
+    return {
+      inputIsInvalid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+  },
+};
+</script>
