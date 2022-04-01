c1ce614ea939eca5e5ca042748b0181ce0f062b4
MacLier <75802094+MacLier@users.noreply.github.com>
2022-04-01 16:22:06 +0200

    feat: app base created

---
+++ Vue-CLI/006 new project from scratch/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul></ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: '1',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: '2',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+<style>
+</style>
