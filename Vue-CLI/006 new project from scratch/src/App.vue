adb231e2e30f6e422a2c4239806aa4d3cafed5bc
MacLier <75802094+MacLier@users.noreply.github.com>
2022-04-04 16:57:23 +0200

    feat: new structural componenets added & setted

--- Vue-CLI/006 new project from scratch/src/App.vue
+++ Vue-CLI/006 new project from scratch/src/App.vue
@@ -1,13 +1,16 @@
 <template>
+  <the-header title="RememeberMe!"></the-header>
   <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
 import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue';
 
 export default {
   components: {
     StoredResources,
+    TheHeader,
   },
   data() {
     return {
@@ -37,5 +40,9 @@ export default {
 }
 html {
   font-family: 'Roboto', sans-serif;
+}
+body {
+  margin: 0;
+  padding: 0;
 }
 </style>
