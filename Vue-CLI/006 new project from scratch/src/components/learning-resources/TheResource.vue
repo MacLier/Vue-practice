7a78213d7d1b726809d9cf26eead51bdaba0e934
MacLier <75802094+MacLier@users.noreply.github.com>
2022-04-05 16:51:30 +0200

    feat: AddResource & TheResource components added & setted

---
+++ Vue-CLI/006 new project from scratch/src/components/learning-resources/TheResource.vue
@@ -0,0 +1,33 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resource')"
+      >Stored Resource</base-button
+    >
+    <base-button @click="setSelectedTab('add-resource')"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+<script>
+
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue'
+import BaseCard from '../UI/BaseCard.vue';
+import BaseButton from '../UI/BaseButton.vue';
+export default {
+  components: { BaseCard, BaseButton, StoredResources, AddResource },
+  data(){
+      return {
+          selectedTab: 'stored-resource';
+      }
+  },
+  methods: {
+      setSelecetedTab(tab){
+          this.setSelecetedTab = tab;
+      }
+  }
+};
+</script>
+<style scoped>
+</style>
