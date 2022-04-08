9d438bc8a619d2cdce2e1e6d7a49ddf970070d77
MacLier <75802094+MacLier@users.noreply.github.com>
2022-04-08 19:11:15 +0200

    feat: GET & POST methoods also connected in Firebase testdata

--- Vue-CLI/008 - httpRequests/src/components/survey/UserExperiences.vue
+++ Vue-CLI/008 - httpRequests/src/components/survey/UserExperiences.vue
@@ -3,7 +3,9 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
       <ul>
         <survey-result
@@ -21,9 +23,35 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch(
+        'https://vue-http-demo-cd926-default-rtdb.europe-west1.firebasedatabase.app/surveys.json'
+      )
+        .then((request) => {
+          return request.json();
+        })
+        .then((data) => {
+          const result = [];
+          for (let id in data) {
+            result.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = result;
+        })
+        .catch((err) => console.log(err));
+    },
   },
 };
 </script>
